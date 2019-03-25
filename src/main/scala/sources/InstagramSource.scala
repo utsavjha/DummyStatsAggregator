65d8c4716c20d5c87dbb0425e061217b0a0f02d2
Zitong Lian <zitong.lian@ing.com>
2019-03-25 12:17:52 +0100

    Add data source

--- src/main/scala/sources/InstagramSource.scala
+++ src/main/scala/sources/InstagramSource.scala
@@ -2,7 +2,7 @@ package sources
 
 class InstagramSource {
   // Create a source of String using the generateInstagramJson function
-  // Update it  every n-seconds with  some simple generation logic
+  // Update it every n-seconds with some simple generation logic
 
   // Use scheduler for pushing data to the Source. Maybe use Source.queue
 }
--- src/main/scala/sources/TwitterSource.scala
+++ src/main/scala/sources/TwitterSource.scala
@@ -1,8 +1,23 @@
 package sources
 
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Sink, Source, SourceQueueWithComplete}
+import akka.stream.{ActorMaterializer, Materializer}
+
 class TwitterSource {
   // Create a source of String using the generateTwitterJson function
-  // Update it  every n-seconds with  some simple generation logic
+  // Update it every n-seconds with some simple generation logic
 
   // Use scheduler for pushing data to the Source. Maybe use Source.queue
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val mat: Materializer = ActorMaterializer()
+  val bufferSize = 100
+
+  val overflowStrategy = akka.stream.OverflowStrategy.dropHead
+
+  val queue: SourceQueueWithComplete[String] = Source.queue(bufferSize, overflowStrategy)
+    .to(Sink foreach println)
+    .run() // in order to "keep" the queue Materialized value instead of the Sink's
+
+  queue offer model.JsonData.generateFacebookJson(2, 3)
 }
