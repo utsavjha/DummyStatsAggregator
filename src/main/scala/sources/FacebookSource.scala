2f61954fce06870a9766442a4da315a7c4be857e
Zitong Lian <zitong.lian@ing.com>
2019-03-25 13:21:00 +0100

    Clean up

--- src/main/scala/sources/FacebookSource.scala
+++ src/main/scala/sources/FacebookSource.scala
@@ -1,9 +1,5 @@
 package sources
 
-import akka.NotUsed
-import akka.stream.scaladsl.{Sink, Source, SourceQueueWithComplete}
-import model._
-
 class FacebookSource {
 
   // Create a source of String using the generateFacebookJson function
@@ -11,25 +7,4 @@ class FacebookSource {
 
   // Use scheduler for pushing data to the Source. Maybe use Source.queue
 
-  val bufferSize = 100
-  val overflowStrategy = akka.stream.OverflowStrategy.dropHead
-  val queue: SourceQueueWithComplete[Nothing] = Source.queue(bufferSize, overflowStrategy)
-    .to(Sink foreach println).run()
-
-
-  queue offer JsonData.generateFacebookJson(3, 2)
-
 }
-//case class Weather(zipCode : String, temperature : Double, raining : Boolean)
-//
-//val bufferSize = 100
-//
-////if the buffer fills up then this strategy drops the oldest elements
-////upon the arrival of a new element.
-//val overflowStrategy = akka.stream.OverflowStrategy.dropHead
-//
-//val queue: SourceQueueWithComplete[Nothing] = Source.queue(bufferSize, overflowStrategy)
-//  .to(Sink foreach println)
-//  .run() // in order to "keep" the queue Materialized value instead of the Sink's
-//
-//queue offer Weather("02139", 32.0, true)
