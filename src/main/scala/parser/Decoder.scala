647e138161bf0bd3cb9d4282d60d3418677ff9d7
yannis <yannis.tzanellis@ing.com>
2019-03-25 10:12:10 +0100

    Init

---
+++ src/main/scala/parser/Decoder.scala
@@ -0,0 +1,6 @@
+package parser
+
+class Decoder {
+ // Decode all input Json to a GenericEntry object
+  // Use circe custom  decoders
+}
---
+++ src/main/scala/sources/InstagramSource.scala
@@ -0,0 +1,8 @@
+package sources
+
+class InstagramSource {
+  // Create a source of String using the generateInstagramJson function
+  // Update it  every n-seconds with  some simple generation logic
+
+  // Use scheduler for pushing data to the Source. Maybe use Source.queue
+}
